7e1a55d64c5f4f279af2123f77c0db923e3a3d68
Sher-Min Yang <sherminyang@gmail.com>
2019-02-16 22:30:35 -0500

    commit

--- server/db/database.js
+++ server/db/database.js
@@ -3,7 +3,7 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
-const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
+const db = new Sequelize(`postgres://localhost:5432/coin-catcher`, {
   logging: false
 })
 
