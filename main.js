34dc171310a6ebe29ab8d67b04a8bf1b55b7aee4
Sher-Min Yang <sherminyang@gmail.com>
2019-02-17 21:10:56 -0500

    taking out db

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // const {db} = require('./server/db')
 const app = require('./server')
 
+app.listen(process.env.PORT || 1337)
 // db.sync()
 //   .then(() => {
 //     app.listen(process.env.PORT || 1337)
